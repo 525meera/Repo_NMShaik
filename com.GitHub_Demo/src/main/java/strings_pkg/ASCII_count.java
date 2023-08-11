025d1c71f2321cf371cb8faf403196f3d425730c
NMShaik <nmshaik525@gmail.com>
2023-08-11 11:44:25 +0530

    commit

--- com.GitHub_Demo/src/main/java/strings_pkg/ASCII_count.java
+++ com.GitHub_Demo/src/main/java/strings_pkg/ASCII_count.java
@@ -1,14 +1,15 @@
 package strings_pkg;
 
-public class ASIQUE_count
+public class ASCII_count
 {
 	public static void main(String[] args) {
-		String str="a b c";//ASIQUE VALUES COUNT
+		String str="abc";//ASCII VALUES COUNT
 		int count=0;
 		char[]ch=str.toCharArray();
 		for(int i=0; i<str.length();i++) 
 		{
-			count=count+ch[i];
+			//count=count+ch[i];    //adding ASCII values
+			count=ch[i];           //actual ASCII values
 			System.out.println(count);
 			
 		}
