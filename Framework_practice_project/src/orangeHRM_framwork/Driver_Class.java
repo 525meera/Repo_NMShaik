a57e61b9201b05a63c8041ea0fccea79f88072e0
NMShaik <nmshaik525@gmail.com>
2023-08-16 18:41:26 +0530

    commit

--- Framework_practice_project/src/orangeHRM_framwork/Driver_Class.java
+++ Framework_practice_project/src/orangeHRM_framwork/Driver_Class.java
@@ -9,7 +9,7 @@ public class Driver_Class
 
 	public static void driver_method() throws InterruptedException 
 	{
-		System.setProperty("webdriver.chrome.driver","D:\\Selenium_jarfiles\\chromedriver-win32\\chromedriver-win32");
+		System.setProperty("webdriver.chrome.driver","D:\\ChromeExe\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.manage().window().maximize();
